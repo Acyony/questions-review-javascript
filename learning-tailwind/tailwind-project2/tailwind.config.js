6bafc7f49d14bf9c37b8fda414c90b689c92a82f
Alcione <alcionefribeiro@gmail.com>
2022-01-19 23:08:15 +0100

    pushing new projects and changes

--- learning-tailwind/tailwind-project2/tailwind.config.js
+++ learning-tailwind/tailwind-project2/tailwind.config.js
@@ -1,6 +1,8 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: false,
         content: [
             "index.html"
         ],
@@ -10,6 +12,23 @@ module.exports = {
         debugScreens: {
             position: ['top', 'left'],
         },
+        colors: {
+            transparent: colors.transparent,
+            current: colors.current,
+            white: colors.white,
+            black: colors.black,
+            blue: colors.blue,
+            yellow: colors.yellow,
+            gray: colors.gray,
+            cyan: colors.cyan,
+            green: colors.lime,
+            red: colors.rose,
+            fuchsia: colors.fuchsia,
+            indigo: colors.indigo,
+            purple: colors.purple,
+            rose: colors.rose,
+            pink: colors.pink,
+        },
         extend: {
             fontFamily: {
                 headline: ['Montserrat-Thin']
@@ -17,7 +36,11 @@ module.exports = {
             colors: {
                 mainColor: '#C2DDDC',
                 titleColor: '#E56997'
+            },
+            spacing: {
+                13: '3.5rem',
             }
+
         },
     },
     plugins: [
